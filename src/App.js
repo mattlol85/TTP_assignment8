d12c964fff8692f32f072576e3bf042cf68d8785
HuiyuGuan <Huiyu.guan20@bcmail.cuny.edu>
2022-01-18 02:02:41 -0500

    added navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,77 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 import Table from "./table";
+import Navbar from './Navbar';
+import Footer from './Footer'
 
 function App() {
   const [color, setColor] = useState("transparent")
-  const[rows, setRows] = useState(1)
+  const [rows, setRows] = useState(1)
   const [cells, setCells] = useState(1)
 
-  function addRow(){
-    setRows(rows+1)
+  function addRow() {
+    setRows(rows + 1)
   }
 
-  function addCells(){
+  function addCells() {
     setCells(cells + 1)
   }
 
-  function clrChange(ev){
-      setColor(ev.target.value)
+  function clrChange(ev) {
+    setColor(ev.target.value)
   }
 
-  function removeRow(){
-      setRows(prevState => prevState - 1)
+  function removeRow() {
+    setRows(prevState => prevState - 1)
   }
 
-  function removeCells(){
-      setCells(prevState => prevState - 1)
+  function removeCells() {
+    setCells(prevState => prevState - 1)
   }
 
-  function clearAll(){
-      setColor("transparent")
-      setCells(1)
-      setRows(1)
+  function clearAll() {
+    setColor("transparent")
+    setCells(1)
+    setRows(1)
   }
 
-    function fillUncolored(ev){
-      let cells = document.getElementsByClassName("cell")
-      for(let i = 0; i < cells.length; i++){
-        if(cells[i].className === "transparent"){
-          cells[i].className = ev.target.value
-        }
+  function fillUncolored(ev) {
+    let cells = document.getElementsByClassName("cell")
+    for (let i = 0; i < cells.length; i++) {
+      if (cells[i].style.backgroundColor === "transparent") {
+        cells[i].style.backgroundColor = setColor(ev.target.value)
       }
-      return cells
     }
+    return cells
+  }
 
-    function fillAll(ev){
-      let cells = document.getElementsByClassName("cell")
-      for(let i = 0; i < cells.length; i++){
-          cells[i].className = ev.target.value
-      }
-      return cells
+  function fillAll(ev) {
+    let cells = document.getElementsByClassName("cell")
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = setColor(ev.target.value)
     }
+    return cells
+  }
 
-  return(
-      <>
-          <button onClick={() => addRow()}>Add row</button>
-          <button onClick={() => addCells()}>Add column</button>
-          <button onClick={() => removeRow()}>Remove Row</button>
-          <button onClick={() => removeCells()}>Remove Column</button>
-          <button onClick={() => fillUncolored()}>Fill all uncolored cells</button>
-          <button onClick={() => fillAll()}>Fill all cells</button>
-          <select id={"selection"} onInput={clrChange}>
-            <option value={"transparent"}>None</option>
-            <option value={"blue"}>Blue</option>
-            <option value={"red"}>Red</option>
-            <option value={"green"}>Green</option>
-            <option value={"yellow"}>Yellow</option>
-          </select>
-          <button onClick={() => clearAll() }>Clear</button>
-            <div>
-              <Table rows={rows} cells={cells} color={color}/>
-            </div>
-      </>
+  return (
+    <>
+      <Navbar />
+      <button onClick={() => addRow()}>Add row</button>
+      <button onClick={() => addCells()}>Add column</button>
+      <button onClick={() => removeRow()}>Remove Row</button>
+      <button onClick={() => removeCells()}>Remove Column</button>
+      <button onClick={() => fillUncolored()}>Fill all uncolored cells</button>
+      <button onClick={() => fillAll()}>Fill all cells</button>
+      <select id={"selection"} onInput={clrChange}>
+        <option value={"transparent"}>None</option>
+        <option value={"blue"}>Blue</option>
+        <option value={"red"}>Red</option>
+        <option value={"green"}>Green</option>
+        <option value={"yellow"}>Yellow</option>
+      </select>
+      <button onClick={() => clearAll()}>Clear</button>
+      <div>
+        <Table rows={rows} cells={cells} color={color} />
+        <Footer />
+      </div>
+    </>
   )
 
 
